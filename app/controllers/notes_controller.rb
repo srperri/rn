9bf3e5a778fb18c83aba16a9a211a3cfe98c27de
Perri, Sebastián <srperri@gmail.com>
2021-02-19 03:29:54 -0300

    nested resources

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,7 +2,7 @@ class NotesController < ApplicationController
   before_action :set_note, only: [:show, :edit, :update, :destroy, :download]
   before_action :set_book , except: [:create]
 
-  include ApplicationHelper
+  include FilenameHelper
   # GET /notes/1
   def show
   end 
@@ -22,7 +22,7 @@ class NotesController < ApplicationController
     @note = @book.notes.new(note_params)
 
     if @note.save
-      redirect_to @note, notice: 'Note was successfully created.'
+      redirect_to [@book, @note], notice: 'Note was successfully created.'
     else
       render :new
     end
@@ -31,7 +31,7 @@ class NotesController < ApplicationController
   # PATCH/PUT /notes/1
   def update
     if @note.update(note_params)
-      redirect_to @note, notice: 'Note was successfully updated.'
+      redirect_to [@book, @note], notice: 'Note was successfully updated.'
     else
       render :edit
     end
@@ -53,7 +53,7 @@ class NotesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_book
-      @book = current_user.books.find(params[:book_id]||@note.book_id)
+      @book = current_user.books.find(params[:book_id])
     end
 
     # Use callbacks to share common setup or constraints between actions.
